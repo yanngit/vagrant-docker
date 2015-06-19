8257c6575514acbe4c46c8fe8ebd6c69564c0813
yann blanc <yann.blanc.38144@gmail.com>
2015-06-19 11:32:45 +0200

    Added nodeRedis project, a simple node application with redis interrogation and beautify code

---
+++ docker-node-redis/app.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+app = express(),
+redis = require('redis').createClient(
+	process.env.REDIS_PORT_6379_TCP_PORT || 6379,
+	process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1',
+	{}
+),
+server = require('http').createServer(app);
+
+app.get('/', function(req, res) {
+  res.json({
+    status: redis ? "ok" : "not ok"
+  });
+});
+var port = process.env.HTTP_PORT || 5000;
+server.listen(port);
+console.log('Listening on port ' + port);
